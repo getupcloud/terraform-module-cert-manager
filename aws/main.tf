e2bcf7829bbf3afe0285535560230b5234c60f26
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2022-11-30 01:04:14 -0300

    wip

--- aws/main.tf
+++ aws/main.tf
@@ -39,6 +39,7 @@ data "aws_iam_policy_document" "aws_certmanager" {
 }
 
 resource "aws_iam_policy" "aws_certmanager" {
+  count = length(var.hosted_zone_ids) > 0 ? 1 : 0
   name        = local.name_prefix
   description = "Cert manager policy for EKS cluster ${var.cluster_name}"
   policy      = data.aws_iam_policy_document.aws_certmanager.json
@@ -52,7 +53,7 @@ module "irsa_aws_certmanager" {
   create_role                   = true
   role_name                     = local.name_prefix
   provider_url                  = var.cluster_oidc_issuer_url
-  role_policy_arns              = [aws_iam_policy.aws_certmanager.arn]
+  role_policy_arns              = length(var.hosted_zone_ids) > 0 ? [aws_iam_policy.aws_certmanager[0].arn] : null
   oidc_fully_qualified_subjects = ["system:serviceaccount:${var.service_account_namespace}:${var.service_account_name}"]
   tags                          = var.tags
 }
